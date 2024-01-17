e19cbc91e04f0612c7b5de303dea4b138ca3bde0
Sander G <sandergarita@gmail.com>
2024-01-17 16:03:55 -0600

    Created HomeScreen

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -5,7 +5,47 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions:  [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: const Icon(Icons.account_circle_rounded,
+            size: 25,
+            color: Colors.white,),
+            onPressed: (){},
+              ),
+              
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: const Icon(Icons.menu,
+            size: 25,
+            color: Colors.white,),
+            onPressed: (){},
+              ),
+              
+          )
+        ],
+        leading: MaterialButton(onPressed: (){
+
+        }, 
+        child: Image.asset("assets/images/ico.png")
+        
+        ,),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(onPressed: (){}, child: const Text("My Music")),
+            TextButton(onPressed: (){}, child: const Text("Analytics")),
+            TextButton(onPressed: (){}, child: const Text("Bank"))
+          ],
+        ),
+      ),
 
     );
   }
